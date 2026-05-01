0ca31f89f38f0c6059a87ad464d832afff2367dd
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-05-01 15:42:40 +0100

    Re-add the buildSrc project; bump version to 2.0.1

---
+++ buildSrc/src/main/kotlin/Library.kt
@@ -0,0 +1,25 @@
+object Library {
+    const val NAME = "DrawBox-Enhanced"
+    const val DESCRIPTION = "Compose Multiplatform drawing library!"
+    const val URL = "https://github.com/CyrusCastle/DrawBox-Enhanced"
+
+    const val GROUP = "uk.codecymru.drawbox"
+    const val VERSION = "2.0.1"
+
+    object License {
+        const val NAME = "Apache-2.0"
+        const val URL = "https://opensource.org/license/apache-2-0/"
+    }
+
+    object OriginalAuthor {
+        const val ID = "MarkYav"
+        const val NAME = "Mark Yavorskyi"
+        const val EMAIL = "yavorskiymark@gmail.com"
+    }
+
+    object Author {
+        const val ID = "CyrusCastle"
+        const val NAME = "Cyrus Castle"
+        const val EMAIL = "cyrus@codecymru.uk"
+    }
+}
