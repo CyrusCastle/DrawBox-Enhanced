a2ac2fed121c1b0c43ac97db3390ba7b0d5987ea
Cyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-05-06 23:34:58 +0100

    Update Drawbox & DrawBoxCanvas to use our new system of emitting the bitmap

--- drawbox/src/commonMain/kotlin/uk/codecymru/drawbox/box/DrawBox.kt
+++ drawbox/src/commonMain/kotlin/uk/codecymru/drawbox/box/DrawBox.kt
@@ -2,20 +2,37 @@ package uk.codecymru.drawbox.box
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import uk.codecymru.drawbox.controller.BitmapDrawController
 import uk.codecymru.drawbox.controller.DrawBoxSubscription
-import uk.codecymru.drawbox.controller.DrawController
 
 @Composable
 fun DrawBox(
-    controller: DrawController,
+    controller: BitmapDrawController,
+    enableInteraction: Boolean = true,
+    subscription: DrawBoxSubscription = DrawBoxSubscription.DynamicUpdate,
     modifier: Modifier = Modifier,
 ) {
-    val bitmap by controller.getBitmap(null, DrawBoxSubscription.DynamicUpdate).collectAsState()
+    var version by remember { mutableStateOf(-1) }
+
+    LaunchedEffect(Unit) {
+        controller.trigger.collect {
+            version = it
+        }
+    }
+
+    val bitmap = controller.internalBitmap
     val background by controller.background.collectAsState()
+
+    val currentAction by controller._currentAction.collectAsState()
+
+    val color by controller.color.collectAsState()
+    val strokeWidth by controller.strokeWidth.collectAsState()
+    val opacity by controller.opacity.collectAsState()
+    val canvasTool by controller.canvasTool.collectAsState()
+
     val canvasOpacity by controller.canvasOpacity.collectAsState()
 
     Box(modifier = modifier) {
@@ -24,8 +41,16 @@ fun DrawBox(
             modifier = Modifier.fillMaxSize(),
         )
         DrawBoxCanvas(
-            bitmap = bitmap,
-            alpha = canvasOpacity,
+            bitmap = bitmap ?: ImageBitmap(1, 1),
+            version = version,
+            subscription = subscription,
+            enableInteraction = enableInteraction,
+            currentAction = currentAction,
+            color = color,
+            strokeWidth = strokeWidth,
+            opacity = opacity,
+            canvasTool = canvasTool,
+            canvasOpacity = canvasOpacity,
             onSizeChanged = controller::connectToDrawBox,
             onTap = controller::onTap,
             onDragStart = controller::onDragStart,
--- drawbox/src/commonMain/kotlin/uk/codecymru/drawbox/box/DrawBoxCanvas.kt
+++ drawbox/src/commonMain/kotlin/uk/codecymru/drawbox/box/DrawBoxCanvas.kt
@@ -1,22 +1,34 @@
 package uk.codecymru.drawbox.box
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntSize
+import uk.codecymru.drawbox.controller.DrawBoxSubscription
+import uk.codecymru.drawbox.model.CanvasTool
 
 @Composable
-fun DrawBoxCanvas(
+internal fun DrawBoxCanvas(
     bitmap: ImageBitmap,
-    alpha: Float,
+    version: Int,
+    subscription: DrawBoxSubscription,
+    enableInteraction: Boolean,
+    currentAction: List<Pair<Offset, Offset>>,
+    color: Color,
+    strokeWidth: Float,
+    opacity: Float,
+    canvasTool: CanvasTool,
+    canvasOpacity: Float,
     onSizeChanged: (IntSize) -> Unit,
     onTap: (Offset) -> Unit,
     onDragStart: (Offset) -> Unit,
@@ -24,20 +36,36 @@ fun DrawBoxCanvas(
     onDragEnd: () -> Unit,
     modifier: Modifier,
 ) {
-    Canvas(modifier = modifier
-        .onSizeChanged(onSizeChanged)
-        .pointerInput(Unit) { detectTapGestures(onTap = onTap) }
-        .pointerInput(Unit) {
-            detectDragGestures(
-                onDragStart = onDragStart,
-                onDrag = { change, _ -> onDrag(change.position) },
-                onDragEnd = onDragEnd,
-                onDragCancel = onDragEnd
+    Box(
+        modifier = modifier
+        .then(other =
+            if (enableInteraction) Modifier
+                .onSizeChanged(onSizeChanged)
+                .pointerInput(Unit) { detectTapGestures(onTap = onTap) }
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDragStart = onDragStart,
+                        onDrag = { change, _ -> onDrag(change.position) },
+                        onDragEnd = onDragEnd,
+                        onDragCancel = onDragEnd
+                    )
+                }
+            else Modifier
+        )
+        .clipToBounds()
+        .alpha(canvasOpacity)
+    ) {
+        key(version) {
+            DrawBoxDisplay(
+                bitmap = bitmap,
+                subscription = subscription,
+                currentAction = currentAction,
+                color = color,
+                strokeWidth = strokeWidth,
+                opacity = opacity,
+                canvasTool = canvasTool,
+                canvasOpacity = canvasOpacity
             )
         }
-        .clipToBounds()
-        .alpha(alpha)
-    ) {
-        drawImage(image = bitmap) // TODO need to think about re-adding "opened image" above here, between the background and the drawn image
     }
 }
