b428381f133d6b02284ed12f30662deeea52fa69
Cyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-05-06 23:53:39 +0100

    Delete old unused classes/helpers

--- drawbox/src/commonMain/kotlin/uk/codecymru/drawbox/model/CanvasAction.kt
+++
@@ -1,6 +0,0 @@
-package uk.codecymru.drawbox.model
-
-sealed class CanvasAction {
-    data class Draw(val path: PathWrapper) : CanvasAction()
-    data class Erase(val erased: List<PathWrapper>) : CanvasAction()
-}
