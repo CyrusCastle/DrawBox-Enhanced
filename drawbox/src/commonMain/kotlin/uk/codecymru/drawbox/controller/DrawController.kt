2fc6a90e5e6c2924d3cebdc1807f858078cc3616
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-05-09 11:14:57 +0100

    Add some very-nothing kdocs for the controller class

--- drawbox/src/commonMain/kotlin/uk/codecymru/drawbox/controller/DrawController.kt
+++ drawbox/src/commonMain/kotlin/uk/codecymru/drawbox/controller/DrawController.kt
@@ -16,6 +16,17 @@ import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
+/**
+ * The DrawController, it contains the bitmap & logic to draw onto bitmap
+ *
+ * @property fillScope A CoroutineScope for the controller to use when doing expensive operations i.e. fill tool
+ * @property startingTool Optional, specifies starting tool out of CanvasTool enum
+ * @property startingOpacity Optional alpha value for brushstrokes
+ * @property startingStrokeWidth Optional, 0f leads to a hairline width
+ * @property startingColor Optional starting colour
+ * @property startingBackground Optional starting background
+ * @property startingCanvasOpacity Optional alpha value for the canvas as a whole
+ */
 class DrawController(
     private val fillScope: CoroutineScope? = null,
     startingTool: CanvasTool = CanvasTool.BRUSH,
