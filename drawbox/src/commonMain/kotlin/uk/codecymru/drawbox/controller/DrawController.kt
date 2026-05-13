44875fa3b017654d64ae88101f4fc2bc0c8e9d6c
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-05-13 10:46:27 +0100

    Move irrelevant top level functions and extension functions out of DrawController; switch currentAction to be a DrawAction rather than path; use new factory

--- drawbox/src/commonMain/kotlin/uk/codecymru/drawbox/controller/DrawController.kt
+++ drawbox/src/commonMain/kotlin/uk/codecymru/drawbox/controller/DrawController.kt
@@ -1,20 +1,25 @@
 package uk.codecymru.drawbox.controller
 
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.ImageBitmapConfig
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import uk.codecymru.drawbox.model.CanvasTool
 import uk.codecymru.drawbox.model.DrawAction
-import uk.codecymru.drawbox.model.DrawnPath
+import uk.codecymru.drawbox.model.DrawActionFactory
 import uk.codecymru.drawbox.model.PaintOptions
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
+import uk.codecymru.drawbox.model.applyTo
 
 /**
  * The DrawController, it contains the bitmap & logic to draw onto bitmap
@@ -50,8 +55,8 @@ class DrawController(
     private var internalCanvas: Canvas = Canvas(internalBitmap)
     private val _actions = MutableStateFlow<List<DrawAction>>(emptyList())
     private val _undoneActions = MutableStateFlow<List<DrawAction>>(emptyList())
-    internal val _currentAction = MutableStateFlow<DrawnPath>(emptyList())
-    private var lastPoint: Offset? = null
+    private val _currentAction = MutableStateFlow<DrawAction?>(null)
+    internal val currentAction = _currentAction.asStateFlow()
 
     ///////////////////
     // DERIVED STATE //
@@ -147,7 +152,7 @@ class DrawController(
     }
 
     /** Clear the canvas and history */
-    fun reset() { // TODO or could reset be an action of its own, to be undone and redone??
+    fun reset() {
         _actions.value = emptyList()
         _undoneActions.value = emptyList()
         redrawHistory()
@@ -181,8 +186,8 @@ class DrawController(
 
                 is DrawAction.Spray -> {
                     action.copy(
-                        points = action.points.filter {
-                            it.x <= maxWidth && it.y <= maxHeight
+                        path = action.path.filter { (from, to) ->
+                            from.x <= maxWidth && from.y <= maxHeight && to.x <= maxWidth && to.y <= maxHeight
                         }
                     )
                 }
@@ -222,53 +227,26 @@ class DrawController(
     internal fun onDragStart(offset: Offset) {
         if (!enabled.value) return
 
-        lastPoint = offset
         _undoneActions.value = emptyList()
-        _currentAction.value = emptyList()
+        _currentAction.value = null
         drawSegment(offset, offset)
     }
 
     internal fun onDrag(offset: Offset) {
         if (!enabled.value) return
 
-        val start = lastPoint ?: offset
+        val start = _currentAction.value?.getFinalPoint() ?: offset
         drawSegment(start, offset)
-        lastPoint = offset
     }
 
     internal fun onDragEnd() {
         if (!enabled.value) return
 
-        if (_currentAction.value.isNotEmpty()) {
-            val paint = PaintOptions(
-                color.value,
-                strokeWidth.value,
-                opacity.value,
-                canvasTool.value
-            ).createPaint()
-
-            val action = when (canvasTool.value){
-                CanvasTool.BRUSH, CanvasTool.ERASER -> DrawAction.Path(
-                    points = _currentAction.value.toList(),
-                    paint = paint
-                )
-                CanvasTool.SPRAY_CAN -> drawSpraySegment(_currentAction.value.toList().map { it.first })
-                CanvasTool.SHAPE_LINE, CanvasTool.SHAPE_RECT, CanvasTool.SHAPE_CIRCLE -> {
-                    DrawAction.Shape(
-                        start = _currentAction.value.first().first,
-                        end = _currentAction.value.first().second,
-                        shapeType = canvasTool.value,
-                        paint = paint
-                    )
-                }
-                else -> DrawAction.Fill(points = emptyList(), color = Color.Black) // TODO not the best way of doing this
-            }
-
-            applyDrawActionToInternalCanvas(action)
-            _actions.value += action
+        if (_currentAction.value != null) {
+            applyDrawActionToInternalCanvas(_currentAction.value!!)
+            _actions.value += _currentAction.value!!
         }
-        lastPoint = null
-        _currentAction.value = emptyList()
+        _currentAction.value = null
     }
 
     internal fun onTap(offset: Offset) {
@@ -287,185 +265,36 @@ class DrawController(
     // PRIVATE DRAWING METHODS //
     /////////////////////////////
 
-    private fun Rect.expandToInclude(other: Rect): Rect {
-        return Rect(
-            minOf(left, other.left),
-            minOf(top, other.top),
-            maxOf(right, other.right),
-            maxOf(bottom, other.bottom)
-        )
-    }
-
     private fun applyDrawActionToInternalCanvas(action: DrawAction){
-        internalCanvas.applyDrawAction(action)
+        action.applyTo(internalCanvas)
         triggerRedraw()
-    }
-
-    private fun Canvas.applyDrawAction(action: DrawAction){
-        when (action) {
-            is DrawAction.Path -> {
-                action.points.forEach { (from, to) ->
-                    this.drawLine(from, to, action.paint)
-                }
-            }
-            is DrawAction.Fill -> {
-                this.drawPoints(
-                    pointMode = PointMode.Points,
-                    points = action.points,
-                    paint = Paint().apply {
-                        color = action.color
-                        strokeWidth = 1f
-                        blendMode = BlendMode.SrcOver
-                        isAntiAlias = false
-                    }
-                )
-            }
-            is DrawAction.Spray -> {
-                this.drawPoints(
-                    pointMode = PointMode.Points,
-                    points = action.points,
-                    paint = action.paint
-                )
-            }
-            is DrawAction.Shape -> {
-                when (action.shapeType){
-                    CanvasTool.SHAPE_LINE -> {
-                        this.drawLine(action.start, action.end, action.paint)
-                    }
-
-                    CanvasTool.SHAPE_RECT -> {
-                        this.drawRect(
-                            left = minOf(action.start.x, action.end.x),
-                            top = minOf(action.start.y, action.end.y),
-                            right = maxOf(action.start.x, action.end.x),
-                            bottom = maxOf(action.start.y, action.end.y),
-                            paint = action.paint
-                        )
-                    }
-
-                    CanvasTool.SHAPE_CIRCLE -> {
-                        this.drawOval(
-                            left = minOf(action.start.x, action.end.x),
-                            top = minOf(action.start.y, action.end.y),
-                            right = maxOf(action.start.x, action.end.x),
-                            bottom = maxOf(action.start.y, action.end.y),
-                            paint = action.paint
-                        )
-
-                    }
-
-                    else -> {}
-                }
-            }
-        }
     }
 
     private fun drawSegment(from: Offset, to: Offset) {
         if (state.value !is DrawBoxConnectionState.Connected) return
 
-        val start = if (_currentAction.value.isEmpty()) lastPoint ?: from else _currentAction.value.first().first
+        val paint = PaintOptions(
+            color.value,
+            strokeWidth.value,
+            opacity.value,
+            canvasTool.value
+        ).createPaint()
+
+        val start = _currentAction.value?.getFinalPoint() ?: from
 
         when (canvasTool.value){
-            CanvasTool.BRUSH, CanvasTool.ERASER, CanvasTool.SPRAY_CAN -> _currentAction.value += from to to // TODO WOULD THIS BE BETTER IF currentAction TOOK A REAL ACTION RATHER THAN WHAT IT DOES NOW?
-            CanvasTool.FILL -> fillScope?.launch { _actions.value += fillSegment(to, color.value) }
-            CanvasTool.EYEDROPPER -> fillScope?.launch { setColorFromPixel(to) }
-            CanvasTool.SHAPE_LINE, CanvasTool.SHAPE_RECT, CanvasTool.SHAPE_CIRCLE -> _currentAction.value = listOf(start to to)
+            CanvasTool.EYEDROPPER -> setColorFromPixel(to)
+            else -> _currentAction.value = DrawActionFactory.extend(_currentAction.value, internalBitmap, canvasTool.value, paint, start, to)
         }
     }
 
-    private fun drawSpraySegment(points: List<Offset>): DrawAction.Spray {
-        val radius = strokeWidth.value
-        val density = (radius * 2).toInt()
-        val sprayPoints = mutableListOf<Offset>()
-
-        points.forEach { (x, y) ->
-            repeat(density) {
-                val angle = (0..360).random().toDouble() * (PI / 180)
-                val distance = (0..100).random().toFloat() / 100f * radius
-
-                val dX = x + (cos(angle) * distance).toFloat()
-                val dY = y + (sin(angle) * distance).toFloat()
-
-                sprayPoints.add(Offset(dX, dY))
-            }
-        }
-
-        val action = DrawAction.Spray(
-            points = sprayPoints,
-            paint = PaintOptions(
-                color.value,
-                1f,
-                opacity.value,
-                canvasTool.value
-            ).createPaint()
-        )
-
-        return action
-    }
-
-    // TODO this no longer needs to be suspend:
-    private suspend fun fillSegment(startOffset: Offset, targetColor: Color): DrawAction.Fill {
-        val action = fillSegment(internalBitmap, startOffset, targetColor)
-        applyDrawActionToInternalCanvas(action)
-
-        return action
-    }
-
-    private fun getPixelArray(bitmap: ImageBitmap): IntArray{
-        val pixels = IntArray(bitmap.width * bitmap.height)
-        bitmap.readPixels(pixels, startX = 0, startY = 0, width = bitmap.width, height = bitmap.height)
-
-        return pixels
-    }
-
-    private fun fillSegment(bitmap: ImageBitmap, startOffset: Offset, targetColor: Color): DrawAction.Fill {
-        val x = startOffset.x.toInt().coerceIn(0, bitmap.width - 1)
-        val y = startOffset.y.toInt().coerceIn(0, bitmap.height - 1)
-
-        val pixels = getPixelArray(bitmap)
-
-        val startColor = pixels[y * bitmap.width + x]
-        if (startColor == targetColor.toArgb()) return DrawAction.Fill(emptyList(), targetColor)
-
-        val fillPoints = mutableListOf<Offset>()
-
-        // Breadth-First Search
-        val queue = ArrayDeque<IntOffset>()
-        queue.add(IntOffset(x, y))
-
-        val visited = BooleanArray(bitmap.width * bitmap.height)
-
-        while (queue.isNotEmpty()) { // TODO following BFS has quite a lot of boxing
-            val curr = queue.removeFirst()
-            val cx = curr.x
-            val cy = curr.y
-
-            if (cx !in 0 until bitmap.width || cy !in 0 until bitmap.height) continue
-            val index = cy * bitmap.width + cx
-
-            if (!visited[index] && pixels[index] == startColor) {
-                visited[index] = true
-                fillPoints.add(Offset(cx.toFloat(), cy.toFloat()))
-
-                queue.add(IntOffset(cx + 1, cy))
-                queue.add(IntOffset(cx - 1, cy))
-                queue.add(IntOffset(cx, cy + 1))
-                queue.add(IntOffset(cx, cy - 1))
-            }
-        }
-
-        return DrawAction.Fill(fillPoints, targetColor)
-    }
-
-    // TODO this doesn't need to be suspend anymore:
-    private suspend fun setColorFromPixel(offset: Offset) {
+    private fun setColorFromPixel(offset: Offset) {
         val x = offset.x.toInt().coerceIn(0, internalBitmap.width - 1)
         val y = offset.y.toInt().coerceIn(0, internalBitmap.height - 1)
 
-        val pixels = getPixelArray(internalBitmap)
+        val pixels = internalBitmap.getPixelArray()
         color.value = Color(pixels[y * internalBitmap.width + x])
     }
-
 
     /////////////////
     // UNDO & REDO //
@@ -521,4 +350,11 @@ class DrawController(
     private fun triggerRedraw(){
         _trigger.tryEmit(_version++)
     }
+}
+
+internal fun ImageBitmap.getPixelArray(): IntArray{
+    val pixels = IntArray(this.width * this.height)
+    this.readPixels(pixels, startX = 0, startY = 0, width = this.width, height = this.height)
+
+    return pixels
 }
